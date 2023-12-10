8f1cbd9943156cceda63f58498fd5cc917b8a199
Will Oliver <willoliver@Wills-Invent-MacBook.local>
2023-12-10 18:50:07 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# ServerPlus
+
+## About
+
+A simple C++ TCP socket server implementation employing a Libevent wrapper for efficient, portable polling. Windows is not currently supported.
+
+## LibeventPlus
+
+The Libevent wrapper is my own. The package is installed as a third-party CMake build.
+See https://github.com/willmroliver/LibeventPlus.
+
---
+++ test/server.cpp
@@ -0,0 +1,42 @@
+#include <boost/test/unit_test.hpp>
+#include <event2/event.h>
+
+#include "client.hpp"
+#include "server.hpp"
+
+#include <thread>
+
+struct GlobalFixture {
+    static test::Client<1024> client;
+    static db::Server s;
+    static std::thread t;
+
+    void setup() {
+        BOOST_TEST_MESSAGE("initializing server");
+
+        auto run = [=] () {
+            s.run();
+        }; 
+        
+        t = std::thread(run);
+        t.detach();
+    }
+
+    void teardown() {
+        s.stop();
+
+        BOOST_ASSERT(s.get_status() != -1);
+        BOOST_TEST_MESSAGE("client, server tests complete");
+    }
+};
+
+test::Client<1024> GlobalFixture::client { "3993" };
+db::Server GlobalFixture::s { "3993" };
+std::thread GlobalFixture::t;
+
+BOOST_TEST_GLOBAL_FIXTURE( GlobalFixture );
+
+BOOST_AUTO_TEST_CASE( simple_handler_test, * boost::unit_test::description("test client connects to server") ) {
+    BOOST_ASSERT(GlobalFixture::client.try_connect());
+    BOOST_ASSERT(GlobalFixture::client.try_close());
+}
