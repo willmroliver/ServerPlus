1f0bef3eef6e7d8a312a3c3820ad769a9cbc9dd8
Will Oliver <willoliver.main@gmail.com>
2023-12-30 12:51:03 +0000

    improve test coverage & debug

--- test/buffer.cpp
+++ test/buffer.cpp
@@ -1,10 +1,16 @@
 #include <boost/test/unit_test.hpp>
-#include <boost/test/parameterized_test.hpp>
-#include <boost/bind/bind.hpp>
 #include <string>
 
 #include "buffer.hpp"
 
+/**
+ * @brief Mocks a call to recvfrom()
+ * 
+ * @param socket 
+ * @param buffer 
+ * @param length 
+ * @return int 
+ */
 int mock_recvfrom(int socket, void* buffer, size_t length) {
     static char data[9] = "12345678";
 
@@ -15,8 +21,37 @@ int mock_recvfrom(int socket, void* buffer, size_t length) {
     return length;
 }
 
+/**
+ * @brief The callback to pass to buffer's write() when reading from the above mock_recvfrom().
+ * 
+ * @param buff 
+ * @param n 
+ * @param data 
+ * @return int 
+ */
 int write_cb(char* buff, unsigned n, void* data) {
     return mock_recvfrom(4, buff, n);
+}
+
+/**
+ * @brief Artificially adds an offset to the buffer's internal pointers.
+ * Useful to test function behaviour & conditions over the looping bound of the internal array.
+ * 
+ * @tparam T The buffer size
+ * @param b The buffer
+ * @param offset The offset size
+ */
+template <unsigned T>
+void offset_buffer(serv::Buffer<T>& b, unsigned offset) {
+    b.write([] (char* dest, unsigned n, void* data) -> int {
+        for (auto i = 0; i < n; ++i) {
+            *(dest + i) = '0' + i % 10;
+        }
+
+        return n;
+    }, offset, nullptr);
+
+    b.read(offset);
 }
 
 struct WriteTestCase {
@@ -92,7 +127,7 @@ BOOST_AUTO_TEST_CASE ( buffer_read_write_test ) {
     BOOST_ASSERT( !can_write );
     BOOST_ASSERT( !buffer.bytes_free() );
 
-    // in buffer: |> ---- ---- ---- ----
+    // in buffer: |>---- ---- ---- ----
     std::string result = buffer.read(100);
     BOOST_ASSERT( result.size() == 16 );
     BOOST_ASSERT( result == "1234567812345678\0" );
@@ -128,4 +163,126 @@ BOOST_AUTO_TEST_CASE ( buffer_read_write_test ) {
     BOOST_ASSERT( buffer.empty() );
     BOOST_ASSERT( buffer.can_write() );
     BOOST_ASSERT( buffer.bytes_free() == 16 );
+}
+
+struct FindTestCase {
+    unsigned offset;
+    std::string initial;
+    std::string search;
+    size_t expecting;
+};
+
+FindTestCase find_tests[] = {
+    {0, "0123456789", "A", std::string::npos},
+    {0, "0123456789", "0", 0},
+    {0, "0123456789", "9", 9},
+    {0, "0123456789", "5", 5},
+    {0, "0123456789", "012", 0},
+    {0, "0123456789", "789", 7},
+    {0, "0123456789", "345", 3},
+    {10, "0123456789", "345", 13},
+    {12, "01234567", "34", 15},
+    {0, "", "", std::string::npos},
+    {16, "", "34", std::string::npos},
+};
+
+void do_find_test(FindTestCase& test) {
+    serv::Buffer<16> buffer;
+
+    // Let's artificially induce an offset in the buffer.
+    if (test.offset) {
+        offset_buffer<16>(buffer, test.offset);
+    }
+
+    buffer.write([] (char* dest, unsigned n, void* data) -> int {
+        auto test = (FindTestCase*) data;
+
+        for (auto i = 0; i < n; ++i) {
+            *(dest + i) = test->initial[0];
+            test->initial.erase(0, 1);
+        }
+
+        return n;
+    },  test.initial.size(), &test);
+
+    auto result = buffer.find(test.search);
+
+    if (result != test.expecting) {
+        std::cout 
+        << "buffer_find_table_test failed: searching for '" 
+        << test.search 
+        << "', expecting "
+        << test.expecting 
+        << ", got " 
+        << result 
+        << std::endl;
+    }
+
+    BOOST_ASSERT( result == test.expecting );
+}
+
+BOOST_AUTO_TEST_CASE( buffer_find_table_test ) {
+    for (auto &test : find_tests) do_find_test(test);
+}
+
+struct ReadToTestCase {
+    unsigned offset;
+    std::string initial;
+    char delim;
+    std::string expecting;
+};
+
+ReadToTestCase read_to_tests[] = {
+    {0, "12345678", '1', ""},
+    {0, "12345678", '2', "1"},
+    {0, "12345678", '9', "12345678"},
+    {0, "12345678", 0, "12345678"},
+    {12, "12345678", '1', ""},
+    {12, "12345678", '2', "1"},
+    {12, "12345678", '9', "12345678"},
+    {12, "12345678", 0, "12345678"},
+    {16, "12345678", '1', ""},
+    {16, "12345678", '2', "1"},
+    {16, "12345678", '9', "12345678"},
+    {16, "12345678", 0, "12345678"},
+};
+
+void do_read_to_test(ReadToTestCase& test) {
+    serv::Buffer<16> buffer;
+
+    if (test.offset) {
+        offset_buffer<16>(buffer, test.offset);
+    }
+
+    buffer.write([] (char* dest, unsigned n, void* data) -> int {
+        auto test = (ReadToTestCase*) data;
+
+        for (auto i = 0; i < n; ++i) {
+            *(dest + i) = test->initial[0];
+            test->initial.erase(0, 1);
+        }
+
+        return n;
+    },  test.initial.size(), &test);
+
+    auto result = buffer.read_to(test.delim);
+
+    if (result != test.expecting) {
+        std::cout 
+        << "buffer_find_table_test failed: reading from '" 
+        << test.initial
+        << " until "
+        << test.delim 
+        << "', expecting "
+        << test.expecting 
+        << ", got " 
+        << result 
+        << std::endl;
+    }
+
+    BOOST_ASSERT( result == test.expecting );
+}
+
+BOOST_AUTO_TEST_CASE( buffer_read_to_table_test ) {
+    for (auto &test : read_to_tests) do_read_to_test(test);
 }
