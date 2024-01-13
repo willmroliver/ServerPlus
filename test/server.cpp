32e24fc968b6e06e64c0ade1d49d695e8561b99d
Will Oliver <willoliver@Wills-Invent-MacBook.local>
2024-01-13 20:44:55 +0000

    implement handshake protocol to perform a diffie-hellman key-exchange

--- test/server.cpp
+++ test/server.cpp
@@ -1,10 +1,11 @@
 #include <boost/test/unit_test.hpp>
-#include <event2/event.h>
+#include <crypt/exchange.hpp>
 
 #include "util.hpp"
 #include "client.hpp"
 #include "server.hpp"
-#include "header.pb.h"
+#include "host-handshake.pb.h"
+#include "peer-handshake.pb.h"
 
 #include <thread>
 #include <iostream>
@@ -39,19 +40,27 @@ std::thread GlobalFixture::t;
 
 BOOST_TEST_GLOBAL_FIXTURE( GlobalFixture );
 
-BOOST_AUTO_TEST_CASE( ping_test, * boost::unit_test::description("test server responses to correct & malformed headers") ) {
+BOOST_AUTO_TEST_CASE( handshake_test, * boost::unit_test::description("test server handshake procedure") ) {
     BOOST_ASSERT(GlobalFixture::client.try_connect());
 
-    serv::proto::Header header;
-    header.set_type("ping");
-    header.set_size(0);
+    serv::proto::HostHandshake host_hs;
 
-    auto time_start = test::Util::current_timestamp<std::chrono::milliseconds>();
-    header.set_timestamp(time_start);
+    BOOST_ASSERT(host_hs.ParseFromString(GlobalFixture::client.try_recv()));
+    auto host_pk_str = host_hs.public_key();
 
-    BOOST_ASSERT(GlobalFixture::client.try_send(header.SerializeAsString()));
-    BOOST_ASSERT(header.ParseFromString(GlobalFixture::client.try_recv()));
-    BOOST_ASSERT(time_start == header.timestamp());
+    crpt::Exchange dh { "ffdhe2048" };
+    crpt::PublicKeyDER host_pk;
+
+    host_pk.from_vector({ host_pk_str.begin(), host_pk_str.end() });
+    serv::proto::PeerHandshake peer_hs;
+
+    auto peer_pk = dh.get_public_key().to_vector();
+    peer_hs.set_public_key({ peer_pk.begin(), peer_pk.end() });
+
+    BOOST_ASSERT(GlobalFixture::client.try_send(peer_hs.SerializeAsString()));
+
+    auto res = GlobalFixture::client.try_recv();
+    BOOST_ASSERT(res.size() == 1 && res[0] == 1);
 
     BOOST_ASSERT(GlobalFixture::client.try_close());
 }
