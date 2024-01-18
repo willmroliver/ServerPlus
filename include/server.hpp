6652274d072fd6aa4156c8ffd4549bd8ad65f5e0
Will Oliver <willoliver.main@gmail.com>
2024-01-18 23:18:34 +0000

    implement thread pool for execution of context callbacks

--- include/server.hpp
+++ include/server.hpp
@@ -7,6 +7,7 @@
 #include <string>
 #include <memory>
 #include "socket.hpp"
+#include "thread-pool.hpp"
 
 using namespace libev;
 
@@ -20,6 +21,7 @@ class Server {
         Socket listen_sock;
         EventBase base;
         std::unordered_map<evutil_socket_t, std::shared_ptr<Context>> ctx_pool;
+        ThreadPool thread_pool;
         int status = 0;
 
         static event_callback_fn accept_callback;
@@ -27,8 +29,9 @@ class Server {
     public:
         Server();
         Server(std::string port);
+        Server(std::string port, unsigned thread_count);
         Server(Server &s) = delete;
-        Server(Server &&s) = default;
+        Server(Server &&s) = delete;
         ~Server();
 
         /**
@@ -60,6 +63,19 @@ class Server {
          * @return int 
          */
         int get_status() const;
+
+        /**
+         * @brief Pass any generic function to the thread pool, to later be executed by a thread, passing in the args given.
+         * 
+         * @tparam F The function type
+         * @tparam Args The function arguments
+         * @param f The function
+         * @param args The arguments to execute the function with.
+         */
+        template <typename F, typename... Args>
+        void allocate_work(F&& f, Args&& ...args) {
+            thread_pool.enqueue(std::forward<F>(f), std::forward<Args>(args)...);
+        }
 };
 
 }
