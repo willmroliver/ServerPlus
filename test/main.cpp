633b1f1f845a8feb06486a2d1e439cfc8fa3ddc6
Will Oliver <willoliver.main@gmail.com>
2024-02-13 22:29:01 +0000

    add socket tests

--- test/main.cpp
+++ test/main.cpp
@@ -1,3 +1,18 @@
 #define BOOST_TEST_MODULE server_test_suite
 
 #include <boost/test/unit_test.hpp>
+#include <fstream>
+#include "logger.hpp"
+
+struct GlobalFixture {
+    
+    std::ofstream tout {};
+    std::ofstream terr {};
+
+    void setup() {
+        serv::Logger::get();
+        serv::Logger::set(&tout, &terr);
+    }
+};
+
+BOOST_TEST_GLOBAL_FIXTURE( GlobalFixture );
