450ff7b4494b9e5f3e282b8a32bb5bddd078cfca
Will Oliver <willoliver.main@gmail.com>
2024-11-24 11:53:14 +0000

    Full support in Socket classes for client applications
    - Sockets now support blocking or non-blocking I/O, depending on requirements of client.
    - Combines with full-support for the peer handshake to provide full-support for a client connection.
    - Library thus supports the Server protocol and Sockets which interact correctly with that protocol, both as host & peer.

--- test/server.cpp
+++ test/server.cpp
@@ -33,31 +33,6 @@ struct ServerFixture {
     }
 };
 
-BOOST_FIXTURE_TEST_CASE( handshake_integration_test, ServerFixture ) {
-    BOOST_ASSERT(client.try_connect());
-
-    serv::proto::HostHandshake host_hs;
-
-    BOOST_ASSERT(host_hs.ParseFromString(client.try_recv()));
-    auto host_pk_str = host_hs.public_key();
-
-    crpt::Exchange dh { "ffdhe2048" };
-    crpt::PublicKeyDER host_pk;
-
-    host_pk.from_vector({ host_pk_str.begin(), host_pk_str.end() });
-    serv::proto::PeerHandshake peer_hs;
-
-    auto peer_pk = dh.get_public_key().to_vector();
-    peer_hs.set_public_key({ peer_pk.begin(), peer_pk.end() });
-
-    BOOST_ASSERT(client.try_send(peer_hs.SerializeAsString()));
-
-    auto res = client.try_recv();
-    BOOST_ASSERT(res.size() == 1 && res[0] == 1);
-
-    BOOST_ASSERT(client.try_close());
-}
-
 BOOST_FIXTURE_TEST_CASE( ping_integration_test, ServerFixture ) {
     client.try_connect();
     client.handshake_init();
